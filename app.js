9dd97ad4b085fba2aede3cde37e3075786c4c66f
Diana <diana.zhusupbekova.kk@gmail.com>
2022-03-19 03:17:10 +0600

    feat: update files

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-document.getElementById("button-2").addEventListener("click", getJson);
-
-function getJson() {
+function cards() {
   fetch("db.json")
     .then((res) => {
       return res.json();
     })
-    .then((data) => {
-      let output = "";
+    .then((product) => {
+      product.forEach((e) => {
+        const card = document.querySelector(".card");
+        const ul = document.createElement("ul");
+        const li = document.createElement("li");
+        const img = document.createElement("img");
+        const hr = document.createElement("hr");
+        const p = document.createElement("h3");
+        const price = document.createElement("h4");
+        const desc = document.createElement("h5");
 
-      data.forEach((post) => {
-        output += `<li>${post.title}</li>`;
+        img.getAttribute("src");
+        img.src = e.img;
+        p.textContent = e.title;
+        price.textContent = e.price;
+        desc.textContent = e.desc;
+        card.append(ul);
+        ul.append(li);
+        li.append(img);
+        li.append(p);
+        li.append(price);
+        li.append(hr);
+        li.append(desc);
       });
-
-      document.getElementById("output").innerHTML = output;
     })
     .catch((error) => {
       console.log(error);
     });
 }
+cards();
